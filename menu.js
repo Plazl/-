1e2655dd9e743c463a1385301c0d0b9f61c10c1c
jhnbg rth <I1234itsplaz@outlook.com>
2022-09-27 11:45:49 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,17 +1,19 @@
-
      function switchPage(num) {
       if(num == 1) {
        document.getElementById("1").style.display = "shown"
        document.getElementById("2").style.display = "none"
        document.getElementById("3").style.display = "none" 
+       console.log(num)
       } else if(num == 2) {
        document.getElementById("2").style.display = "shown"
        document.getElementById("1").style.display = "none"
        document.getElementById("3").style.display = "none"
+       console.log(num)
       } else if(num == 3) {
        document.getElementById("3").style.display = "shown"
        document.getElementById("2").style.display = "none"
        document.getElementById("1").style.display = "none"
+       console.log(num)
       }
      }
     
