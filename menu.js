52a6f183cb00b54f3f1bf2bd5a3b0e57bddb5275
jhnbg rth <I1234itsplaz@outlook.com>
2022-09-29 11:52:35 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,26 +1,24 @@
-var x;
-function setNum(num) {
- x = num;
+var openTab = 1
+var x1 = document.getElementById("1")
+var x2 = document.getElementById("2")
+var x3 = document.getElementById("3")
+function pageSwitch(num) {
+ openTab = num
 }
-function pageSwitch() {      
-      if(x == 1) {
-       document.getElementById("1").style.display = "shown"
-       document.getElementById("2").style.display = "none"
-       document.getElementById("3").style.display = "none" 
-       console.log(document.getElementById("1").style.display)
-       console.log(x)
-    } else if(x == 2) {
-       document.getElementById("2").style.display = "shown"
-       document.getElementById("1").style.display = "none"
-       document.getElementById("3").style.display = "none"
-       console.log(document.getElementById("2").style.display)
-       console.log(x)
-    } else if(x == 3) {
-       document.getElementById("3").style.display = "shown"
-       document.getElementById("2").style.display = "none"
-       document.getElementById("1").style.display = "none"
-       console.log(document.getElementById("3").style.display)
-       console.log(x)
-      }
-     }
-    setInterval(pageSwitch(),10)
+setInterval(function(){
+  if (openTab == 1) {
+   x1.style.display = "block"
+} else {
+   x1.style.display = "none"
+}
+ if (openTab == 2) {
+   x2.style.display = "block"
+} else {
+   x2.style.display = "none"
+}
+ if (openTab == 3) {
+   x3.style.display = "block"
+} else {
+   x3.style.display = "none"
+ }
+}); 
