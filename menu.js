fb8299443c1ddee8531585c150585b10fad247fc
jhnbg rth <I1234itsplaz@outlook.com>
2022-09-29 11:18:53 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,20 +1,26 @@
 var x;
-function pageSwitch(x) {
+function setNum(num) {
+ x = num;
+}
+function pageSwitch() {      
       if(x == 1) {
        document.getElementById("1").style.display = "shown"
        document.getElementById("2").style.display = "none"
        document.getElementById("3").style.display = "none" 
+       console.log(document.getElementById("1").style.display)
        console.log(x)
-      } else if(x == 2) {
+    } else if(x == 2) {
        document.getElementById("2").style.display = "shown"
        document.getElementById("1").style.display = "none"
        document.getElementById("3").style.display = "none"
+       console.log(document.getElementById("2").style.display)
        console.log(x)
-      } else if(x == 3) {
+    } else if(x == 3) {
        document.getElementById("3").style.display = "shown"
        document.getElementById("2").style.display = "none"
        document.getElementById("1").style.display = "none"
+       console.log(document.getElementById("3").style.display)
        console.log(x)
       }
      }
-    
+    setInterval(pageSwitch(),10)
